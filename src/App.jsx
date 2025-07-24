319021737d3d805ca58d725d9e3319a5877493e7
Alsanoce <40732771+Alsanoce@users.noreply.github.com>
2025-07-24 11:53:20 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,21 @@
 import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
-import home from "./pages/home";
-import donate from "./pages/donate";
-import Confirm from "./pages/OtpConfirmationPage";
+import Home from "./pages/home";
+import Donate from "./pages/donate";
+import OtpConfirmationPage from "./pages/OtpConfirmationPage";
 import ThankYou from "./pages/ThankYou";
-import about from "./pages/about";
+import About from "./pages/about";
 
 function App() {
   return (
     <>
       <Navbar />
       <Routes>
-        <Route path="/" element={<home />} />
-        <Route path="/donate" element={<donate />} />
-        <Route path="/OtpConfirmationPage.jsx" element={<OtpConfirmationPage />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/donate" element={<Donate />} />
+        <Route path="/confirm" element={<OtpConfirmationPage />} />
         <Route path="/thank-you" element={<ThankYou />} />
-        <Route path="/about" element={<about />} />
+        <Route path="/about" element={<About />} />
       </Routes>
     </>
   );
