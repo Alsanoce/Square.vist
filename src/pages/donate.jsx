728b82336a9d8ebac91eb75474e7426d24018b0e
Alsanoce <40732771+Alsanoce@users.noreply.github.com>
2025-08-02 10:44:01 +0200

    Update donate.jsx

--- src/pages/donate.jsx
+++ src/pages/donate.jsx
@@ -1,10 +1,9 @@
 import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import { collection, getDocs } from "firebase/firestore";
-import PropTypes from 'prop-types'; 
+import { collection, getDocs } from "firebase/firestore";; 
 import { db } from "../firebase";
 import axios from "axios";
-
+import PropTypes from "prop-types";
 
 function DonateForm() {
   const [phone, setPhone] = useState("");
