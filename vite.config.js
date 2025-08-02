1c824a09738e10be54dd74604c54d94b9264172e
Alsanoce <alsanoce.saad@gmail.com>
2025-08-02 13:29:55 +0200

    تحديث إعدادات vite و package.json

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,11 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      external: ['prop-types'], // أضف الحزم التي تريد استبعادها
-      output: {
-        globals: {
-          'prop-types': 'PropTypes' // تعريف المتغير العام
-        }
-      }
-    }
-  },
-  resolve: {
-    alias: {
-      'prop-types': '/node_modules/prop-types/prop-types.min.js' // مسار مباشر
+      external: ['axios']
     }
   }
-});
+})
