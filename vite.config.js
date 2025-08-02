f7884523710f15af18c7fe20b62dff32a60a5074
Alsanoce <40732771+Alsanoce@users.noreply.github.com>
2025-08-02 10:49:26 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      external: ['prop-types']
+      external: ['prop-types','react-otp-input']
     }
   },
   server: {
