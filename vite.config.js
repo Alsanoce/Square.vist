4f8759aa0a13e70dae90c66537949adc65551d47
Alsanoce <40732771+Alsanoce@users.noreply.github.com>
2025-08-02 13:51:11 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      external: ['axios']
+      external: ['axios'],
+      output: {
+        globals: {
+          'axios': 'axios' // هذا يحدد المتغير العام لـ axios
+        }
+      }
+    }
+  },
+  resolve: {
+    alias: {
+      'axios': '/node_modules/axios/dist/axios.min.js' // تحديد المسار المطلق
     }
   }
-})
+});
