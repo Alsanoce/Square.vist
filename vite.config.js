cf4db99be86eb0442097fd003645017e34be7ce1
Alsanoce <40732771+Alsanoce@users.noreply.github.com>
2025-08-02 12:25:28 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,10 +5,17 @@ export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      external: ['prop-types', 'react-otp-input']
+      external: ['prop-types'], // أضف الحزم التي تريد استبعادها
+      output: {
+        globals: {
+          'prop-types': 'PropTypes' // تعريف المتغير العام
+        }
+      }
     }
   },
-  optimizeDeps: {
-    include: ['prop-types']
+  resolve: {
+    alias: {
+      'prop-types': '/node_modules/prop-types/prop-types.min.js' // مسار مباشر
+    }
   }
 });
