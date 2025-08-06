91c9518f08339820072b2157cf96a5d42d805fcb
Alsanoce <40732771+Alsanoce@users.noreply.github.com>
2025-08-06 11:08:03 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,14 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    include: ['axios']
-  },
-  build: {
-    commonjsOptions: {
-      include: [/axios/, /node_modules/]
+  resolve: {
+    alias: {
+      'uuid': 'uuid/dist/esm-browser/index.js'
     }
+  },
+  optimizeDeps: {
+    include: ['uuid']
   }
-});
+})
