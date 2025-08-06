be4e80de57adde973fb732c0ccc4692b62d53549
Alsanoce <40732771+Alsanoce@users.noreply.github.com>
2025-08-06 12:01:03 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,14 +2,4 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: ['uuid'],
-      output: {
-        globals: {
-          'uuid': 'uuid'
-        }
-      }
-    }
-  }
-})
+});
