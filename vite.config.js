a70ab4f8d8251cc8beacb33c96164f8a6d03f788
Alsanoce <40732771+Alsanoce@users.noreply.github.com>
2025-08-06 11:45:14 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      'uuid': 'uuid/dist/esm-node/index.js' // تغيير المسار هنا
+  build: {
+    rollupOptions: {
+      external: ['uuid'],
+      output: {
+        globals: {
+          'uuid': 'uuid'
+        }
+      }
     }
   }
 })
