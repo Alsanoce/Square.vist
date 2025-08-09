e77abdc8d1a7e9562497e9130ddc815903d1a8c5
Alsanoce <40732771+Alsanoce@users.noreply.github.com>
2025-08-09 11:46:05 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,16 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:5000', // غيره حسب منفذ الباك إند عندك
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/api/, '/api')
+      }
+    }
+  }
 });
