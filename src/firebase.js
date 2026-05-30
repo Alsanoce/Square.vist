bc5276fa77d134b174a29c435810397a3f65b6be
Alsanoce <40732771+Alsanoce@users.noreply.github.com>
2026-05-30 11:58:07 +0200

    Update Firebase storage bucket URL

--- src/firebase.js
+++ src/firebase.js
@@ -7,7 +7,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyBqCAPMe0ROdSXzdsuGZZ6PRwmEo6RypSg",
   authDomain: "whater-f15d4.firebaseapp.com",
   projectId: "whater-f15d4",
-  storageBucket: "whater-f15d4.firebasestorage.app",
+  storageBucket: "whater-f15d4.appspot.com",
   messagingSenderId: "875744692410",
   appId: "1:875744692410:web:4647c93c7835c61bdc1a83"
 };
